75dd06eba837e3523d019c170534d53fe3d9a616
zainalsalamun <zainalrtf@gmail.com>
2025-12-01 16:18:57 +0700

    update : main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // lib/main.dart
 import 'package:coffe_mobile_apps/modules/home/home_page.dart';
 import 'package:coffe_mobile_apps/modules/menu/equipment_page.dart';
+import 'package:coffe_mobile_apps/services/hive_service.dart';
 import 'package:flutter/material.dart';
 import 'core/theme/app_theme.dart';
 import 'modules/auth/login_page.dart';
@@ -10,7 +11,9 @@ import 'modules/menu/persediaan_page.dart';
 import 'modules/profile/edit_profile_page.dart';
 import 'modules/splash/splash_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await HiveService.init();
   runApp(const MyApp());
 }
 
@@ -28,9 +31,9 @@ class MyApp extends StatelessWidget {
 
       routes: {
         '/': (context) => const SplashPage(),
-        '/login': (context) => const LoginPage(),
-        '/register': (context) => const RegisterPage(),
-        '/home': (context) => const HomePage(),
+        '/login': (context) => LoginPage(),
+        '/register': (context) => RegisterPage(),
+        '/home': (context) => HomePage(),
         '/edit-profile': (context) => const EditProfilePage(),
         '/menu': (context) => const MenuPage(),
         '/equipment': (context) => const EquipmentPage(),
